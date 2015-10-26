a392814f6e17e3e6ec54e5e83d9535d0e5aca257
Marc Farra <marcfarra@gmail.com>
2015-10-26 15:05:06 -0400

    consumer for full diff timeline

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -32,6 +32,12 @@ function processRecord (record) {
   // Only process ways
   var ways = R.filter(R.propEq('type', 'way'), elements);
 
+  var geojsonDiff = {
+    'type': 'FeatureCollection',
+    'features': [],
+    'properties': obj.metadata
+  };
+
   ways.forEach(function (way) {
     var tags = R.keys(way.tags);
     // Process buildings
@@ -50,10 +56,14 @@ function processRecord (record) {
     pipeline.lpush('ogp:timeline:' + user, JSON.stringify(way));
     pipeline.ltrim('ogp:timeline:' + user, 1000);
 
-    // Add way to global timeline
-    pipeline.lpush('ogp:timeline', JSON.stringify(way));
-    pipeline.ltrim('ogp:timeline', 1000);
+    // Process to geojson
+    var geojsonWay = toGeojson(way);
+    geojsonDiff.features.push(geojsonWay);
   });
+
+  // Add changeset to global timeline
+  pipeline.lpush('ogp:timeline', JSON.stringify(geojsonDiff));
+  pipeline.ltrim('ogp:timeline', 100);
 
   // Add num_changes to global count
   pipeline.zincrby('ogp:changes', obj.metadata.num_changes, user);
@@ -62,4 +72,36 @@ function processRecord (record) {
   pipeline.exec(function (err, results) {
     if (err) console.error(err);
   });
+}
+
+function toGeojson (diffEl) {
+  var properties = {};
+  properties.id = diffEl.id;
+  properties.timestamp = diffEl.timestamp;
+  properties.changeset = diffEl.changeset;
+  properties.user = diffEl.user;
+  properties.tags = diffEl.tags;
+
+  var geo = {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'LineString',
+      'coordinates': []
+    },
+    'properties': properties
+  };
+  if (diffEl.action === 'create' || diffEl.action === 'modify') {
+    var nodelist = diffEl.nodes.map(function (node) {
+      return [node.lon, node.lat];
+    });
+    var first = nodelist[0];
+    var last = nodelist[nodelist.length - 1];
+    if (first[0] === last[0] && first[1] === last[1]) {
+      geo.geometry.coordinates = [nodelist];
+      geo.geometry.type = 'Polygon';
+    } else {
+      geo.geometry.coordinates = nodelist;
+    }
+  }
+  return geo;
 }
