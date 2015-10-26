b24b33bbc1390e377912d40fd8c24fc4d5d5d9f7
Marc Farra <marcfarra@gmail.com>
2015-10-26 14:51:51 -0400

    Show me the way for individual edits

--- static/process_timeline.js
+++ static/process_timeline.js
@@ -2,7 +2,37 @@
 function preprocessTimeline (timelineArray) {
   // It's an array of Redis Strings
   var timeline = timelineArray.map(JSON.parse);
-  timeline.forEach(function (element) {
-    console.log(element);
-  });
+  return timeline.map(toGeojson);
+}
+
+function toGeojson (diffEl) {
+  var properties = {};
+  properties.id = diffEl.id;
+  properties.timestamp = diffEl.timestamp;
+  properties.changeset = diffEl.changeset;
+  properties.user = diffEl.user;
+  properties.tags = diffEl.tags;
+
+  var geo = {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'LineString',
+      'coordinates': [],
+    },
+    'properties': properties
+  };
+  if (diffEl.action === 'create' || diffEl.action === 'modify') {
+    var nodelist = diffEl.nodes.map(function (node) {
+      return [node.lon, node.lat];
+    });
+    var first = nodelist[0];
+    var last = nodelist[nodelist.length - 1];
+    if (first[0] === last[0] && first[1] === last[1]) {
+      geo.geometry.coordinates = [nodelist];
+      geo.geometry.type = 'Polygon';
+    } else {
+      geo.geometry.coordinates = nodelist;
+    }
+  }
+  return geo;
 }
