d2f52f47ac78c1c90cd97bb01a7938bd56094959
Marc Farra <marcfarra@gmail.com>
2015-10-26 15:45:56 -0400

    Frontend code for full changesets

--- process_timeline.js
+++ process_timeline.js
@@ -1,38 +1,24 @@
 /*  Preprocess timeline */
 function preprocessTimeline (timelineArray) {
-  // It's an array of Redis Strings
-  var timeline = timelineArray.map(JSON.parse);
-  return timeline.map(toGeojson);
-}
-
-function toGeojson (diffEl) {
-  var properties = {};
-  properties.id = diffEl.id;
-  properties.timestamp = diffEl.timestamp;
-  properties.changeset = diffEl.changeset;
-  properties.user = diffEl.user;
-  properties.tags = diffEl.tags;
-
-  var geo = {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'LineString',
-      'coordinates': [],
-    },
-    'properties': properties
-  };
-  if (diffEl.action === 'create' || diffEl.action === 'modify') {
-    var nodelist = diffEl.nodes.map(function (node) {
-      return [node.lon, node.lat];
-    });
-    var first = nodelist[0];
-    var last = nodelist[nodelist.length - 1];
-    if (first[0] === last[0] && first[1] === last[1]) {
-      geo.geometry.coordinates = [nodelist];
-      geo.geometry.type = 'Polygon';
-    } else {
-      geo.geometry.coordinates = nodelist;
+  var temp = [];
+  var hashes = {};
+  timelineArray.map(JSON.parse).forEach(function (element) {
+    if (!hashes[element.properties.id]) {
+      temp.push(element);
+      hashes[element.properties.id] = 1;
     }
-  }
-  return geo;
+  });
+
+  timelineArray = temp;
+  // It's an array of Redis Strings
+  return timelineArray
+  .sort(function (a, b) {
+    var date1 = new Date(a.properties.created_at);
+    var date2 = new Date(b.properties.created_at);
+
+    if (date1 > date2) return -1;
+    else if (date1 < date2) return 1;
+    else return 0;
+  });
 }
+
