1162ec3a3ef0fa2876b24e355d7534652e3f04e5
Dylan <Dylan@developmentseed.org>
2015-10-27 07:42:45 -0400

    Majority of Style

--- static/main.js
+++ static/main.js
@@ -1,16 +1,14 @@
 /*global L, $, preprocessTimeline*/
-var root = 'http://45.55.146.128:8080';
+var root = 'http://45.55.146.128:8081';
 var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/devseed.07f51987/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q', {
 //    maxZoom: 2,
     minZoom: 2,
-    attribution: "© <a href='https://www.mapbox.com/map-feedback/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap contributors</a>"
 });
 
 var map = L.map('map', { zoomControl: false })
     .addLayer(mapboxTiles)
     .setView([18.025966, -5], 2)
-    .setMaxBounds([ [89, -180], [-89, 180] ])
-    ; 
+    .setMaxBounds([ [89, -180], [-89, 180] ]);
 
 var geojsonLayer = L.geoJson().addTo(map);
 
@@ -63,6 +61,11 @@ function render (element) {
   geojsonLayer.clearLayers();
   geojsonLayer.addData(element);
   map.fitBounds(geojsonLayer.getBounds());
+ if (map.getZoom() > 16){
+    map.setZoom(16)
+    console.log("away we go!")
+}
+
 
   $('#editor_name').empty();
   $('#editor_name').append("Contributions from <h1>" + element.properties.user +"</h1>");
@@ -82,32 +85,44 @@ function render (element) {
 function fillLeaderboard (hash) {
   $('#leaderboard').empty();
   $.get(root + '/' + hash, function (data) {
-    for (var i = 0; i < data.length; i += 2) {
-      var rank = (i/2)+1;
+    for (var i = 2; i < data.length; i += 2) {
+      var rank = (i/2);
+
+      username = data[i]
+      if(data[i].length>20){
+        username = username.substring(0, 17) + "..."
+      }
 
       $('#leaderboard').append(
-        '<li><h1>' + rank+"&middot</h1>  "+ data[i] + '-' + data[i + 1] + '</li>'
+        '<li><h1>' + rank+".</h1>  "+ username + ' <i>' + data[i + 1] + '</i></li>'
       );
     }
+    $('#Total').append(
+      '<li><h1>Total Contributions:</h1><i> '+ data[1] +'</i></li>'
+    );
   });
 }
 
 $('#Leaderboard-All').click(function () {
+  $("#Total").empty()
   fillLeaderboard('changes');
   return $('#leadertitletext').text("LEADERBOARDS");
 });
 
 $('#Leaderboard-Building').click(function () {
+  $("#Total").empty()
   fillLeaderboard('buildings');
   return $('#leadertitletext').text("BUILDINGS");
 });
 
 $('#Leaderboard-Roads').click(function () {
+  $("#Total").empty()
   fillLeaderboard('highways');
   return $('#leadertitletext').text("ROADS");
 });
 
 $('#Leaderboard-Rivers').click(function () {
+  $("#Total").empty()
   fillLeaderboard('waterways');
   return $('#leadertitletext').text("RIVERS");
 });
